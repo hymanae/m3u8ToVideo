d5019b39ba709cba1cf261112630211443290519
Alan Man <alan.man@arch-partnership.com>
2022-04-27 10:29:21 +0800

    revised

--- m3u8tovideo.py
+++ m3u8tovideo.py
@@ -1,43 +1,47 @@
-import os, requests
+import os, glob, ffmpeg
+from os import mkdir, path, chdir
+from shutil import rmtree
 
-index_file_path = input("Enter Index file path with extension m3u8 : ")
+def convert_m3u8_to_mp4(m3u8_file_path, dest):
+    input_stream = ffmpeg.input(m3u8_file_path)
+    output_stream = ffmpeg.output(input_stream, dest, format='mp4')
+    ffmpeg.run(output_stream)
 
-index_file = open(index_file_path,'r')
+def merge_m3u8_contents(index_file_path: str, m3u8_contents_file_path):
+    tmp_paths = index_file_path.split('/')
+    merged_filename = 'merged_' +  tmp_paths.pop()
+    merged_m3u8_file_path = path.join('/'.join(tmp_paths), merged_filename)
 
-indexes = index_file.read()
+    index_file = open(index_file_path, 'r')
+    indexes = index_file.read()
+    index_file.close()
 
-index_file.close()
-
-output_file_path = input("Enter output file path : ")
-
-output_file = open(output_file_path,'wb')
-
-folder_path = input("Enter folder path with extension m3u8_contents ('#<internet>' for get file from internet) : ")
-
-if folder_path == '#<internet>' :
-
-    indexes = indexes.split('http')[1:]
-
-    indexes = ['http'+x.split('\n')[0] for x in indexes]
-
-    for index in indexes :
-
-        content = requests.get(index)
-
-        output_file.write(content.content)
-
-else :
-
+    output_file = open(merged_m3u8_file_path, 'wb')
     indexes = indexes.split('file:')[1:]
-
     indexes = [x.split('\n')[0].split('/')[-1] for x in indexes]
 
-    for index in indexes :
-
-        content_file = open(os.path.join(folder_path,index),'rb')
-
+    for index in indexes:
+        content_file = open(os.path.join(m3u8_contents_file_path, index), 'rb')
         output_file.write(content_file.read())
-
         content_file.close()
 
-output_file.close()
+    output_file.close()
+
+    return merged_m3u8_file_path
+
+
+
+chdir(root_path)
+filenames = [f.split('.')[0] for f in glob.glob("*.m3u8")]
+for filename in filenames:
+    print('processing ' + filename)
+    index_file_path = path.join(root_path, filename) + '.m3u8'
+    contents_path = path.join(root_path, filename) + '.m3u8_contents'
+    m3u8_file_path = merge_m3u8_contents(index_file_path, contents_path)
+    if not os.path.exists(path.join(root_path, 'output')):
+        mkdir(path.join(root_path, 'output'))
+    mp4_file_path =  path.join(root_path, 'output', filename) + '.mp4'
+    convert_m3u8_to_mp4(m3u8_file_path, mp4_file_path)
+
+
+
